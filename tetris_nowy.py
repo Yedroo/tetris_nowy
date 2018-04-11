d76b57e94513c0a845df7920cec81f9d401eb599
Yedroo <38240651+Yedroo@users.noreply.github.com>
2018-04-11 09:57:19 +0200

    Update tetris_nowy.py

--- tetris_nowy.py
+++ tetris_nowy.py
@@ -47,10 +47,10 @@ GREEN = (0, 187, 0)
 GRAY = (225, 225, 225)
 
 DISPLAY_SURFACE.fill(GRAY)
-pygame.draw.line(DISPLAY_SURFACE, BLACK, (60, 60), (120, 60), 4)
-pygame.draw.line(DISPLAY_SURFACE, WHITE, (60, 70), (120, 70), 4)
-pygame.draw.circle(DISPLAY_SURFACE, WHITE, (300, 50), 20, 0)
-pygame.draw.rect(DISPLAY_SURFACE, GREEN, (200, 150, 100, 50))
+#pygame.draw.line(DISPLAY_SURFACE, BLACK, (60, 60), (120, 60), 4)
+#pygame.draw.line(DISPLAY_SURFACE, WHITE, (60, 70), (120, 70), 4)
+#pygame.draw.circle(DISPLAY_SURFACE, WHITE, (300, 50), 20, 0)
+pygame.draw.rect(DISPLAY_SURFACE, GREEN, (int(len(width))/2, 0, 250, 250))
 
 while True:
     for event in pygame.event.get():
